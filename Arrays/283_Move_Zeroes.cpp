214a1e4323b1dc40cc8e64954bf979af3d4416c6
shivamkumar <shivamkr1321@gmail.com>
2026-06-08 16:08:33 +0530

    283

---
+++ Arrays/283_Move_Zeroes.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int n = nums.size();
+
+        int j = -1;
+
+        // Find first zero
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == 0) {
+                j = i;
+                break;
+            }
+        }
+
+        // No zero found
+        if (j == -1)
+            return;
+
+        // Move non-zero elements forward
+        for (int i = j + 1; i < n; i++) {
+            if (nums[i] != 0) {
+                swap(nums[i], nums[j]);
+                j++;
+            }
+        }
+    }
+};
+
+int main() {
+    Solution obj;
+
+    vector<int> nums = {0, 1, 0, 3, 12};
+
+    obj.moveZeroes(nums);
+
+    cout << "After moving zeroes: ";
+    for (int num : nums) {
+        cout << num << " ";
+    }
+
+    cout << endl;
+    return 0;
+}
