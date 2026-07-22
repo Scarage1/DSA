d2110cef5875cada1030f00863cdf4dd5fa6f2df
shivamkumar <shivamkr1321@gmail.com>
2026-07-22 13:39:33 +0530

    392

---
+++ Strings/392_Is_Subsequence.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+#include<iostream>
+using namespace std;
+
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+
+        int i = 0;
+        int j = 0;
+
+        while (i < s.length() && j < t.length()) {
+
+            if (s[i] == t[j]) {
+                i++;
+            }
+
+            j++;
+        }
+
+        return i == s.length();
+    }
+};
+
+int main() {
+
+    Solution obj;
+
+    string s = "abc";
+    string t = "ahbgdc";
+
+    if (obj.isSubsequence(s, t))
+        cout << "True" << endl;
+    else
+        cout << "False" << endl;
+
+    return 0;
+}
